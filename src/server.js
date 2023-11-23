f65e49def6221e3e08c6407e191374424fe718e0
woooooooojin <kevin2205@naver.com>
2023-11-23 16:58:38 +0900

    server setting

--- src/server.js
+++ src/server.js
@@ -1,12 +1,26 @@
 import express from "express";
+import http from "http";
+import WebSocket from "ws"
 
 const app = express()
 
 app.set("view engine","pug")
 app.set("views",__dirname + "/views")
-app.get("/", (req,res)=>res.render("home"))
 app.use("/public", express.static(__dirname + "/public"))
-
+app.get("/", (_,res)=>res.render("home"))
+app.get("/*",(_,res)=>res.redirect("/"))
 
 const handelListen = ()=>console.log(`Listening on http://localhost:3000`)
-app.listen(3000, handelListen)
+// app.listen(3000, handelListen)
+
+const server = http.createServer(app)
+
+const wss = new WebSocket.Server({server})
+
+function handelConnection(socket){
+    console.log(socket)
+}
+
+wss.on('connection',handelConnection)
+
+server.listen(3000, handelListen)
