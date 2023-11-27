bdc8434e026057b0a5f581daa7c63be3490c5403
woooooooojin <kevin2205@naver.com>
2023-11-27 11:46:17 +0900

    websocket event

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,29 @@
+const messageList = document.querySelector("ul")
+const messageForm = document.querySelector('form')
+
 const socket = new WebSocket(`ws://${window.location.host}`)
+
+
+
+socket.addEventListener('open',()=>{
+    console.log('connected to server')
+})
+
+socket.addEventListener('message',(message)=>{
+    console.log('New message : ',message.data)
+})
+
+socket.addEventListener('close',()=>{
+    console.log('Disconnected to server X')
+})
+
+
+function handleSubmit(e){
+    e.preventDefault()
+    const input = messageForm.querySelector('input')
+    socket.send(input.value)
+    input.value = ""
+}
+messageForm.addEventListener('submit',handleSubmit)
+
+
--- src/server.js
+++ src/server.js
@@ -17,10 +17,16 @@ const server = http.createServer(app)
 
 const wss = new WebSocket.Server({server})
 
-function handelConnection(socket){
-    console.log(socket)
-}
+const sockets = [];
 
-wss.on('connection',handelConnection)
+wss.on('connection',(socket)=>{
+    sockets.push(socket)
+    console.log('Connected to Browser')
+    socket.on("close",()=>console.log('Disconnected from the Browser'))
+    socket.on("message",(message)=>{
+        sockets.forEach((aSocket)=>aSocket.send(message.toString()))
+        // socket.send(message.toString('utf8'))
+    })
+})
 
 server.listen(3000, handelListen)
